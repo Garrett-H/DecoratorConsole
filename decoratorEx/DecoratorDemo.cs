438e844ea02cc601c79405416781e0de1aeb0d17
Garrett Hay <garlhay@ut.utm.edu>
2018-10-07 18:33:57 -0500

    Made the implements of classes. Made Main in DecoratorDemo

---
+++ decoratorEx/DecoratorDemo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace decoratorEx
+{
+    class DecoratorDemo
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Starting Main");
+
+            Widget text = new TextField();
+            text.Draw();
+            Console.WriteLine();
+            Decorator Border = new BorderDecorator(text);
+            Border.Draw();
+            Console.WriteLine();
+            Decorator Scroll = new ScrollDecorator(text);
+            Scroll.Draw();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ decoratorEx/Widget.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace decoratorEx
+{
+    interface Widget
+    {
+        void Draw();
+    }
+
+    class TextField : Widget
+    {
+        public void Draw()
+        {
+            Console.WriteLine("Creating TextField");
+        }
+    }
+
+    abstract class Decorator : Widget
+    {
+        private Widget wid;
+
+        public Decorator(Widget w)
+        {
+            this.wid = w;
+            Console.WriteLine("Decorator Beginning");
+        }
+
+        public abstract void Draw();
+    }
+
+    class BorderDecorator : Decorator
+    {
+        private Widget wid;
+        public BorderDecorator(Widget w) : base(w)
+        {
+            Console.WriteLine("Beginning Border Decorator");
+        }
+
+        public override void Draw()
+        {
+            Console.WriteLine("Drawing Border");
+        }
+    }
+
+    class ScrollDecorator : Decorator
+    {
+        public ScrollDecorator(Widget w) : base(w)
+        {
+            Console.WriteLine("Beginning Scroll Decorator");
+        }
+
+        public override void Draw()
+        {
+            Console.WriteLine("Drawing Scroll");
+        }
+    }
+}
